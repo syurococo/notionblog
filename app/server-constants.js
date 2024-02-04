7c8852afbc12bdb027f0a280c4faf8c8166ea438
syurococo <158216254+syurococo@users.noreply.github.com>
2024-02-04 15:56:49 +0900

    Update server-constants.js
    Change Title and Caption

--- app/server-constants.js
+++ app/server-constants.js
@@ -5,8 +5,8 @@ const NOTION_API_SECRET = process.env.NOTION_API_SECRET
 const DATABASE_ID = process.env.DATABASE_ID
 const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL
 const NEXT_PUBLIC_GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID
-const NEXT_PUBLIC_SITE_TITLE = 'easy-notion-blog'
-const NEXT_PUBLIC_SITE_DESCRIPTION = 'Easy to start your blog. You can write on your Notion.'
+const NEXT_PUBLIC_SITE_TITLE = '日本脱出'
+const NEXT_PUBLIC_SITE_DESCRIPTION = '2023年末に日本を脱出してから始めたブログ。YouTubeやSNSでは言えない内容をこちらに書き貯めます。日本で生きるのが大変な人は参考にしてください'
 const NUMBER_OF_POSTS_PER_PAGE = 10
 
 module.exports = {
